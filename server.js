c461eae3dd1ec8a3ae1e586ef57a5f2b2f8ef819
Nasir Ahmed Raffick <nasir@ccs.neu.edu>
2016-05-21 21:50:39 -0400

    added page-create.html,page-edit.html,page-list.html,website-create.html,website-edit.html,website-list.html,widget-list.html

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.use(express.static(__dirname + '/public'));
 require ("./test/app.js")(app);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 3001;
 
 app.listen(port, ipaddress);
