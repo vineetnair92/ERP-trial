e391ea870cb44b5cf2b79c0adc9f9c8a0b6f06b8
nasatgithub <nasir@ccs.neu.edu>
2016-05-27 22:21:11 -0400

    Renamed all the view files to end with view.client.html, modified the views so that it can be added to ng-view div. Added app.js, config.js and LoginController.

---
+++ public/assignment/views/user/login.controller.client.js
@@ -0,0 +1,15 @@
+/**
+ * Created by nasir on 5/27/16.
+ */
+(function() {
+    console.log("HELLOOOOOOOOOOOOOOOO");
+    angular.module("WebAppMaker")
+        .controller("LoginController", LoginController);
+    
+    function LoginController() {
+        var cModel = this;
+   
+
+    }
+    
+})();
