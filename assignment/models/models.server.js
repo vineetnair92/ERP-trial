424bb69714b2ac467afb40588bfd248c1486800c
nasatgithub <nasir@ccs.neu.edu>
2016-06-11 16:45:09 -0400

    added user models and website models, testing delete functionalities

--- assignment/models/models.server.js
+++ assignment/models/models.server.js
@@ -1,7 +1,7 @@
 module.exports = function () {
 
     var mongoose = require("mongoose");
-    mongoose.connect('mongodb://localhost/assignment');
+    mongoose.connect('mongodb://127.0.0.1:27017/assignment');
     var models = {
         userModel: require("./user/user.model.server.js")(),
         websiteModel: require("./website/website.model.server")()
