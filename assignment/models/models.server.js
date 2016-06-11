8db0b33f6b370e1e6fe5f708a88a314a4d4d5f0c
nasatgithub <nasir@ccs.neu.edu>
2016-06-11 16:50:41 -0400

    added user models and website models, testing delete functionalities

--- assignment/models/models.server.js
+++ assignment/models/models.server.js
@@ -1,7 +1,20 @@
 module.exports = function () {
 
     var mongoose = require("mongoose");
-    mongoose.connect('mongodb://127.0.0.1:27017/assignment');
+
+    var connectionString = 'mongodb://localhost/assignment';
+
+    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.OPENSHIFT_APP_NAME;
+    }
+
+    mongoose.connect(connectionString);
+
+
     var models = {
         userModel: require("./user/user.model.server.js")(),
         websiteModel: require("./website/website.model.server")()
